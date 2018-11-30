6d026660fc46c1ae02ccf7a1cc958ac4e14f5cb4
unknown <akira32810@gmail.com>
2018-11-30 17:34:27 -0500

    putting in git from TFS

---
+++ BackupBigFilesData/BackupBigFilesData/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace BackupBigFilesData
+{
+    class Program
+    {
+        //get backup path + file for backup
+        //get destination path + (subfolder)/nameof file.
+
+        static void Main(string[] args)
+        {
+
+            var configFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "backupConfig.txt");
+            var logFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "backupLog.log");
+            writeDefaultTextFile(configFile);
+            //read from configure text path
+            string BackUpConfig = configFile;
+            string BackUpPath = File.ReadLines(configFile).Skip(1).First();
+            int chunkSize = Convert.ToInt32(File.ReadLines(configFile).Skip(2).First());
+            string OutputBackup = File.ReadLines(configFile).Skip(3).First();
+
+
+
+            SplittingFiles.SplitFile(@"" + BackUpPath + "", chunkSize, OutputBackup);
+            Console.WriteLine("Finish backing up files");
+
+            // Console.WriteLine(Environment.SpecialFolder.ApplicationData);
+            //  Console.Write(DateTime.Now.ToString("MM-dd-yyyy"));
+            //CombineFiles.CombineMultipleFilesIntoSingleFile(@"C:\Temp\splitfiles\11-29-2018", "*.vmbkp", @"C:\Temp\joinfiles\testjoin.zip");
+            Console.WriteLine("You may check your backup log in: " + logFile);
+            Console.Read();
+        }
+
+        private static void writeDefaultTextFile(string configFile)
+        {
+          
+            if (!File.Exists(configFile))
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine("#after this line, please put the following: #line2: BackupPath, #line3: chunksize in megabytes to let you know how large each file need to be split, #line4: outputbackupPath");
+                sb.AppendLine("/home/yourprofile/backupfolder");
+                sb.AppendLine("100");
+                sb.AppendLine("/home/yourprofile/outputfolderForSplitFiles");
+                File.WriteAllText(configFile, sb.ToString());
+
+                Console.WriteLine("your config file is created in : " + configFile);
+                Console.WriteLine("You may now exit this app and fix your config file before running this app again");
+                Console.Read();
+                Environment.Exit(0);
+
+            }
+            else
+            {
+                Console.WriteLine("Your config is now being use and located in: " + configFile);
+                //Console.ReadLine();
+            }
+
+            
+           
+
+        }
+
+    }
+}
---
+++ BackupBigFilesData/BackupBigFilesData/SplittingFiles.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace BackupBigFilesData
+{
+    class SplittingFiles
+    {
+        public static void SplitFile(string inputFolder, int chunkSize, string OutPutPath)
+        {
+            //chunksize in megabytes
+            var fileStatusCopyAt = string.Empty;
+            var inputPositionLengh = string.Empty; ;
+            var backupLog = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "backupLog.log");
+            //get all files in folder
+            try
+            {
+                List<string> filesInFolder = GetallFilesinFolder(inputFolder);
+                string DateFormatSubPath = DateTime.Now.ToString("MM-dd-yyyy_T_hhmmss");
+                int fileNum = 0;
+                foreach (var inputFile in filesInFolder)
+                {
+
+                    fileStatusCopyAt = inputFile;
+
+
+                    const int BUFFER_SIZE = 20 * 1024;
+                    byte[] buffer = new byte[BUFFER_SIZE];
+
+                    var backupDir = Path.Combine(OutPutPath, DateFormatSubPath, "file" + fileNum);
+                    //var backupDir = OutPutPath + "/" + DateTime.Now.ToString("MM-dd-yyyy");
+                    if (!Directory.Exists(backupDir))
+                    {
+                        Directory.CreateDirectory(backupDir);
+                    }
+
+                    using (Stream input = File.OpenRead(inputFile))
+                    {
+                        int index = 0;
+                        while (input.Position < input.Length)
+                        {
+                            inputPositionLengh = input.Position.ToString();
+
+
+                            var fileCreatedForBackup = Path.Combine(backupDir, "file" + fileNum + "-part_" + index + "-OrgExt_" + Path.GetExtension(inputFile) + "_" + Guid.NewGuid() + ".vmbkp");
+
+                            // using (Stream output = File.Create(fileCreatedForBackup))
+                            using (FileStream output = File.Create(fileCreatedForBackup))
+                            {
+                                float remaining = chunkSize * 1024f * 1024f;
+                                float totalSize = remaining;
+                                int bytesRead;
+                                Console.WriteLine("\rWriting file: " + fileCreatedForBackup + " ");
+
+                                while (remaining > 0 && (bytesRead = input.Read(buffer, 0,
+                                       BUFFER_SIZE)) > 0)
+                                {
+                                    output.Write(buffer, 0, bytesRead);
+                                    remaining -= bytesRead;
+                                    var percentage = Math.Round((1.00 - ((double)remaining / totalSize)) * 100, 2);
+                                    //Console.SetCursorPosition(0, 2);
+
+                                    Console.Write("\r{0}%  ", percentage);
+                                    //  System.Threading.Thread.Sleep(1);
+
+                                }
+
+
+                            }
+
+                            index++;
+
+                            Thread.Sleep(500); // experimental; perhaps try it
+                        }
+                    }
+                    fileNum++;
+
+                }
+                Console.Clear();
+                Console.WriteLine("");
+
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine(DateTime.Now.ToString());
+                sb.AppendLine("There are no errors");
+                sb.AppendLine("__________________________________________________");
+                File.AppendAllText(backupLog, sb.ToString());
+            }
+
+            catch (Exception ex)
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine(DateTime.Now.ToString());
+                sb.AppendLine("error at file: " + fileStatusCopyAt);
+                sb.AppendLine("Position: " + inputPositionLengh);
+                sb.AppendLine("error: " + ex.Message);
+                sb.AppendLine("__________________________________________________");
+                File.AppendAllText(backupLog, sb.ToString());
+            }
+
+
+        }
+
+        private static List<string> GetallFilesinFolder(string folderPath)
+        {
+            List<string> myfiles = new List<string>();
+            try
+            {
+
+                string[] files = Directory.GetFiles(folderPath,
+            "*.*",
+            SearchOption.AllDirectories);
+
+                foreach (string file in files)
+                {
+                    myfiles.Add(file);
+                }
+
+            }
+            catch (System.Exception excpt)
+            {
+                Console.WriteLine(excpt.Message);
+            }
+
+            return myfiles;
+        }
+    }
+}
